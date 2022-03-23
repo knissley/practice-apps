89b44ec7181975146356f8f651d8a0114e590e71
Kyle Nissley <knissley97@gmail.com>
2022-03-23 09:55:58 -0700

    Set up environment

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,20 @@
-require("dotenv").config();
 const express = require("express");
-const path = require("path");
-
 const app = express();
+require('dotenv').config();
+const path = require("path");
+const port = process.env.PORT || 3000;
+
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
-app.listen(process.env.PORT);
-console.log(`Listening at http://localhost:${process.env.PORT}`);
+app.listen(port);
+console.log(`Listening at http://localhost:${port}`);
